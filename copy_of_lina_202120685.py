610ef2911ba6479e74612970962c92f6f29cbe9a
asdgwkwmk-max <asdgwkwmk@gmail.com>
2026-05-08 17:51:41 +0300

    Update copy_of_lina_202120685.py

--- copy_of_lina_202120685.py
+++ copy_of_lina_202120685.py
@@ -4,11 +4,11 @@ import plotly.express as px
 
 st.title("Netflix Content Analysis - Phase 1")
 
-# تحميل البيانات
+
 try:
     df = pd.read_csv('netflix_titles.csv')
     
-    # رسمة بيانية بسيطة واحترافية
+   
     fig = px.pie(df, names='type', title='Distribution of Movies vs TV Shows')
     st.plotly_chart(fig)
     
